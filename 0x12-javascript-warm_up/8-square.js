7ec4b6a26040b20bf35ab6896157bb8b6e7fb018
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-08-19 20:33:27 +0300

    fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,8 +2,9 @@
 const argSize = Number(process.argv[2]);
 
 if (Number(argSize)) {
-  for (let i = 0; i < argSize; i++)
+  for (let i = 0; i < argSize; i++) {
     console.log('X'.repeat(argSize));
+  }
 } else {
   console.log('Missing size');
 }
