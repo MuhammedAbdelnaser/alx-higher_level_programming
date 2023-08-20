e30a342bfdc14c9bcac8f1d5a2a745fa096bd8e4
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-08-20 19:27:54 +0300

    fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,12 +3,12 @@ const SquareMain = require('./5-square');
 
 class Square extends SquareMain {
   charPrint (c) {
-    if (c == undefined) {
+    if (c === undefined) {
       c = 'X';
     }
-    for (let i = 0; i < this.size; i++) {
+    for (let i = 0; i < this.height; i++) {
       let sum = '';
-      for (let i = 0; i < this.size; i++) {
+      for (let i = 0; i < this.width; i++) {
         sum += c;
       }
       console.log(sum);
