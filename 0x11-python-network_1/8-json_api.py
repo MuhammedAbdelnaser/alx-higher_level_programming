f161bdf41af52b84c8a0ec1640d01cbc76905ddf
MuhammedAbdelnaser <mohamedelreety2014@gmail.com>
2023-12-07 18:19:57 +0200

    fix api json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,11 +4,13 @@
 - sends POST request to http://0.0.0.0:5000/search_user
 with the letter as a parameter.
 """
-import sys
-import requests
 
 
 if __name__ == "__main__":
+    import sys
+    import requests
+
+
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
 
