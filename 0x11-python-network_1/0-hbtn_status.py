dfa826bfd5fd5d5736e7540cdd2f362c72dc65aa
MuhammedAbdelnaser <mohamedelreety2014@gmail.com>
2023-12-07 17:29:27 +0200

    fix status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
+"""
+A python script that fetches and displays a website
 """
 
-
 if __name__ == '__main__':
+    import urllib.parse
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    url = 'https://alx-intranet.hbtn.io/status'
+    req = urllib.request.Request(url)
+
+    with urllib.request.urlopen(req) as page:
+        html = page.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(str(html, "utf-8")))
