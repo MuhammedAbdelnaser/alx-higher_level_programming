170951144965e7d03532b1f8ca360cc01ba15ac5
MuhammedAbdelnaser <mohamedelreety2014@gmail.com>
2023-12-07 17:32:15 +0200

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,16 +5,20 @@
 - takes email as a parameter
 - displays the body of the response
 """
-import sys
-import urllib.parse
-import urllib.request
+if __name__ == '__main__':
+    import sys
+    import urllib.request
+    import urllib.parse
 
-
-if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    mail = sys.argv[2]
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    value = {'email': mail}
+
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        page = response.read()
+
+    print(page.decode('utf-8'))
